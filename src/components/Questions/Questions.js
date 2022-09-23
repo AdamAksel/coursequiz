1d5577306bbf24d068ff1144dcc1688d1f352275
AdamAksel <adamcarlssoncoding@gmail.com>
2022-09-23 12:49:54 +0200

    rethinking project

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -2,24 +2,24 @@
 variables schema:
 
 {
-        questionSort: Variables,
+        questionSort: ``,
         question: ``,
         rightAnswer: [``],
         wrongAnswers: [``,``,``],
         comment: ``
-}
+},
 */
 
-export const variables = [
+export const questions = [
   {
-    questionSort: 'Variables',
+    questionSort: 'Array',
     question: `"let myVariable;" what value will this variable have?`,
     rightAnswer: [`Undefined`],
     wrongAnswers: [`Null`, `Nothing at all`, `String`],
     comment: `A declared but unassigned variable will always have "undefined" as its value.`,
   },
   {
-    questionSort: 'Variables',
+    questionSort: 'Array',
     question: `What is the only reason to use the "var" keyword?`,
     rightAnswer: [`It's supported by older browsers.`],
     wrongAnswers: [`You want to make the variable global.`],
